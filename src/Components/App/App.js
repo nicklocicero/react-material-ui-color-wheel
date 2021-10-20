5c55f14c78441086e6bfb563f6a506280aa8cb6d
Nicholas LoCicero <nick@Nicholass-MacBook-Air-2.local>
2021-10-20 16:07:38 -0500

    react color fun with material ui

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './../../logo.svg';
 import './App.css';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { Button, Paper } from '@material-ui/core';
+import { Button, Paper, Card, CardHeader } from '@material-ui/core';
+
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const colors = ["White", "Tomato", "Orange", "LemonChiffon", "MediumSeaGreen", "DodgerBlue", "SlateBlue", "Violet", "Black"]
   return (
-    <Container maxWidth="sm" className="App">
+    <Container maxWidth="sm" className="App" style={{backgroundColor: colors[Math.abs(count) % colors.length],}}>
       <Paper>
-        <img src={logo} className="App-logo" alt="logo" />
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App + Material-UI
+          Increment Progress
         </Typography>
-        <Button variant="contained" color="primary">
-          Primary Button
+        <Card style={{backgroundColor: colors[Math.abs(count) % colors.length],}}>
+          <CardHeader title={count} style={(8 === count % (colors.length)) ? {color: 'white'} : {color: 'black'}} />
+        </Card>
+        <Button variant="contained" color="secondary" onClick={() => (setCount(count - 1))}>
+          Minus
         </Button>
-        <Button variant="contained" color="secondary">
-          Secondary Button
+        <Button variant="contained" color="primary" onClick={() => (setCount(count + 1))}>
+          Plus
         </Button>
+        <img src={logo} className="App-logo" alt="logo" />
       </Paper>
     </Container>
   );
 }
+
 export default App;
